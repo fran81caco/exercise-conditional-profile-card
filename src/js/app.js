a4aea6b4684c5cefba0a06be56feadf8a0617251
fran81caco <cacodreamer81@gmail.com>
2026-04-07 19:53:52 +0000

    listo

--- src/js/app.js
+++ src/js/app.js
@@ -7,42 +7,63 @@ import "../style/index.css";
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+        avatarURL: "https://i.pravatar.cc/300?img=70&v=4", // this is the url for the profile avatar
         socialMediaPosition: "position-right", // social media bar position (position-left or position-right)
         //for social media links, only update usernames
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
+        twitter: "fran_dev", // social media usernames
+        github: "fran_fullstack",
+        linkedin: "francisco-developer",
+        instagram: "fran.codes",
 
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
+        name: "Francisco Javier",
+        lastName: "González García",
+        role: "Full Stack Developer",
+        country: "Spain",
+        city: "Málaga",
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  const coverHtml = variables.includeCover
+    ? `<div class="cover"><img src="${variables.background ||
+        "https://picsum.photos/600/300"}" /></div>`
+    : "";
 
   // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+  document.querySelector("#widget_content").innerHTML = `
+          <div class="widget">
+          ${coverHtml}
+          <img src="${variables.avatarURL ||
+            "https://i.pravatar.cc/300?img=12"}" class="photo" />
+
+          <h1 style="color:${variables.fontColor || "black"};">
+            ${variables.name || "NAME"} ${variables.lastName || ""}
+          </h1>
+
+          <h2 style="color:${variables.fontColor || "black"};">
+            ${variables.role || "Role"}
+          </h2> 
+
+            <h3 style="color:${variables.fontColor || "black"};">
+            ${variables.city || "City"}, ${variables.country || "Country"}
+         </h3>
+
+         <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }" target="_blank"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }" target="_blank"><i class="fa fa-instagram"></i></a></li>
+
           </ul>
         </div>
+
     `;
 }
 
@@ -56,7 +77,7 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://i.pravatar.cc/300?img=12",
     // social media bar position (position-left or position-right)
     socialMediaPosition: "position-right",
     // social media usernames
@@ -67,25 +88,29 @@ window.onload = function() {
     name: null,
     lastName: null,
     role: null,
-    country: null,
-    city: null
+    country: "Spain",
+    city: "Malaga"
   };
   render(window.variables); // render the card for the first time
 
+  const handlePickerUpdate = function(e) {
+    const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+    let values = {};
+    values[attribute] =
+      this.value == "" || this.value == "null"
+        ? null
+        : this.value == "true"
+        ? true
+        : this.value == "false"
+        ? false
+        : this.value;
+
+    render(Object.assign(window.variables, values)); // render again the card with new values
+  };
+
   document.querySelectorAll(".picker").forEach(function(elm) {
-    elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
-      let values = {};
-      values[attribute] =
-        this.value == "" || this.value == "null"
-          ? null
-          : this.value == "true"
-          ? true
-          : this.value == "false"
-          ? false
-          : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
-    });
+    // update in real time while typing and also support selects/check-like changes
+    elm.addEventListener("input", handlePickerUpdate);
+    elm.addEventListener("change", handlePickerUpdate);
   });
 };
